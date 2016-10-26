db2925e04a3b1867da8f9fa3ebc1db430a0c98d1
marwanasfi <marwanasfi@Marwas-MacBook-Pro.local>
2016-10-26 16:59:51 +0200

    mobile menu + google analytics
    mobile menu icon and links are functional

--- js/main.js
+++ js/main.js
@@ -144,153 +144,180 @@ $(document).on('click', '#skills4', function(){
 //////////////mobile menu icon  //////////////////
 
 (function() {
-  /* In animations (to close icon) */
+ /* In animations (to close icon) */
 
-  var beginAC = 80,
-  endAC = 320,
-  beginB = 80,
-  endB = 320;
+ var beginAC = 80,
+     endAC = 320,
+     beginB = 80,
+     endB = 320;
 
-  function inAC(s) {
-    s.draw('80% - 240', '80%', 0.3, {
-      delay: 0.1,
-      callback: function() {
-        inAC2(s)
-      }
-    });
-  }
+ function inAC(s) {
+     s.draw('80% - 240', '80%', 0.3, {
+         delay: 0.1,
+         callback: function() {
+             inAC2(s)
+         }
+     });
+ }
 
-  function inAC2(s) {
-    s.draw('100% - 545', '100% - 305', 0.6, {
-      easing: ease.ease('elastic-out', 1, 0.3)
-    });
-  }
+ function inAC2(s) {
+     s.draw('100% - 545', '100% - 305', 0.6, {
+         easing: ease.ease('elastic-out', 1, 0.3)
+     });
+ }
 
-  function inB(s) {
-    s.draw(beginB - 60, endB + 60, 0.1, {
-      callback: function() {
-        inB2(s)
-      }
-    });
-  }
+ function inB(s) {
+     s.draw(beginB - 60, endB + 60, 0.1, {
+         callback: function() {
+             inB2(s)
+         }
+     });
+ }
 
-  function inB2(s) {
-    s.draw(beginB + 120, endB - 120, 0.3, {
-      easing: ease.ease('bounce-out', 1, 0.3)
-    });
-  }
+ function inB2(s) {
+     s.draw(beginB + 120, endB - 120, 0.3, {
+         easing: ease.ease('bounce-out', 1, 0.3)
+     });
+ }
 
-  /* Out animations (to burger icon) */
+ /* Out animations (to burger icon) */
 
-  function outAC(s) {
-    s.draw('90% - 240', '90%', 0.1, {
-      easing: ease.ease('elastic-in', 1, 0.3),
-      callback: function() {
-        outAC2(s)
-      }
-    });
-  }
+ function outAC(s) {
+     s.draw('90% - 240', '90%', 0.1, {
+         easing: ease.ease('elastic-in', 1, 0.3),
+         callback: function() {
+             outAC2(s)
+         }
+     });
+ }
 
-  function outAC2(s) {
-    s.draw('20% - 240', '20%', 0.3, {
-      callback: function() {
-        outAC3(s)
-      }
-    });
-  }
+ function outAC2(s) {
+     s.draw('20% - 240', '20%', 0.3, {
+         callback: function() {
+             outAC3(s)
+         }
+     });
+ }
 
-  function outAC3(s) {
-    s.draw(beginAC, endAC, 0.7, {
-      easing: ease.ease('elastic-out', 1, 0.3)
-    });
-  }
+ function outAC3(s) {
+     s.draw(beginAC, endAC, 0.7, {
+         easing: ease.ease('elastic-out', 1, 0.3)
+     });
+ }
 
-  function outB(s) {
-    s.draw(beginB, endB, 0.7, {
-      delay: 0.1,
-      easing: ease.ease('elastic-out', 2, 0.4)
-    });
-  }
+ function outB(s) {
+     s.draw(beginB, endB, 0.7, {
+         delay: 0.1,
+         easing: ease.ease('elastic-out', 2, 0.4)
+     });
+ }
 
-  /* Awesome burger default */
+ /* Awesome burger default */
 
-  var pathA = document.getElementById('pathA'),
-  pathB = document.getElementById('pathB'),
-  pathC = document.getElementById('pathC'),
-  segmentA = new Segment(pathA, beginAC, endAC),
-  segmentB = new Segment(pathB, beginB, endB),
-  segmentC = new Segment(pathC, beginAC, endAC),
-  trigger = document.getElementById('menu-icon-trigger'),
-  toCloseIcon = true,
-  dummy = document.getElementById('dummy'),
-  wrapper = document.getElementById('menu-icon-wrapper');
+ var pathA = document.getElementById('pathA'),
+   pathB = document.getElementById('pathB'),
+   pathC = document.getElementById('pathC'),
+   segmentA = new Segment(pathA, beginAC, endAC),
+   segmentB = new Segment(pathB, beginB, endB),
+   segmentC = new Segment(pathC, beginAC, endAC),
+   trigger = document.getElementById('menu-icon-trigger'),
+   toCloseIcon = true,
+   dummy = document.getElementById('dummy'),
+   wrapper = document.getElementById('menu-icon-wrapper');
 
-  wrapper.style.visibility = 'visible';
+ wrapper.style.visibility = 'visible';
 
-  trigger.onclick = function() {
-    if (toCloseIcon) {
-      inAC(segmentA);
-      inB(segmentB);
-      inAC(segmentC);
+ trigger.onclick = function() {
+   if (toCloseIcon) {
+     inAC(segmentA);
+     inB(segmentB);
+     inAC(segmentC);
 
-      dummy.className = 'dummy dummy--active';
-    } else {
-      outAC(segmentA);
-      outB(segmentB);
-      outAC(segmentC);
+     dummy.className = 'dummy dummy--active';
+     $(".menu_icon").css({'height':'100%','width':'100%', 'background-color':'#fff'});
+     $("body").css({'overflow-y':'hidden'});
+     $(".dummy").css({'display':'block'});
 
-      dummy.className = 'dummy';
-    }
-    toCloseIcon = !toCloseIcon;
-  };
 
-  /* Scale functions */
+     function mobile_menu() {
+       $(".dummy").css({'display':'none'});
+       $(".menu_icon").css({'height':'25%','width':'25%', 'background-color':'transparent'});
+       $("body").css({'overflow-y':'auto'});
+       outAC(segmentA);
+       outB(segmentB);
+       outAC(segmentC);
+          dummy.className = 'dummy';
+          toCloseIcon = !toCloseIcon;
+     }
 
-  function addScale(m) {
-    m.className = 'menu-icon-wrapper scaled';
-  }
+     document.getElementById('mobile_menu_about').onclick = function() { mobile_menu() ; }
+     document.getElementById('mobile_menu_skills').onclick = function() { mobile_menu() ; }
+     document.getElementById('mobile_menu_work').onclick = function() { mobile_menu() ; }
+     document.getElementById('mobile_menu_contact').onclick = function() { mobile_menu() ; }
 
-  function removeScale(m) {
-    m.className = 'menu-icon-wrapper';
-  }
+   } else {
+     outAC(segmentA);
+     outB(segmentB);
+     outAC(segmentC);
 
-  /* Awesome burger scaled */
+     dummy.className = 'dummy';
+     $(".menu_icon").css({'height':'25%','width':'25%', 'background-color':'transparent'});
+     $("body").css({'overflow-y':'auto'});
+     $(".dummy").css({'display':'none'});
+   }
+   toCloseIcon = !toCloseIcon;
 
-  var pathD = document.getElementById('pathD'),
-  pathE = document.getElementById('pathE'),
-  pathF = document.getElementById('pathF'),
-  segmentD = new Segment(pathD, beginAC, endAC),
-  segmentE = new Segment(pathE, beginB, endB),
-  segmentF = new Segment(pathF, beginAC, endAC),
-  wrapper2 = document.getElementById('menu-icon-wrapper2'),
-  trigger2 = document.getElementById('menu-icon-trigger2'),
-  toCloseIcon2 = true,
-  dummy2 = document.getElementById('dummy2');
+ };
 
-  wrapper2.style.visibility = 'visible';
+ /* Scale functions */
 
-  trigger2.onclick = function() {
-    addScale(wrapper2);
-    if (toCloseIcon2) {
-      inAC(segmentD);
-      inB(segmentE);
-      inAC(segmentF);
+ function addScale(m) {
+   m.className = 'menu-icon-wrapper scaled';
+ }
 
-      dummy2.className = 'dummy dummy--active';
-    } else {
-      outAC(segmentD);
-      outB(segmentE);
-      outAC(segmentF);
+ function removeScale(m) {
+   m.className = 'menu-icon-wrapper';
+ }
 
-      dummy2.className = 'dummy';
-    }
-    toCloseIcon2 = !toCloseIcon2;
-    setTimeout(function() {
-      removeScale(wrapper2)
-    }, 450);
-  };
+
+  	/* Awesome burger scaled */
+
+  	var pathD = document.getElementById('pathD'),
+  		pathE = document.getElementById('pathE'),
+  		pathF = document.getElementById('pathF'),
+  		segmentD = new Segment(pathD, beginAC, endAC),
+  		segmentE = new Segment(pathE, beginB, endB),
+  		segmentF = new Segment(pathF, beginAC, endAC),
+  		wrapper2 = document.getElementById('menu-icon-wrapper2'),
+  		trigger2 = document.getElementById('menu-icon-trigger2'),
+  		toCloseIcon2 = true,
+  		dummy2 = document.getElementById('dummy2');
+
+  	wrapper2.style.visibility = 'visible';
+
+  	trigger2.onclick = function() {
+  		addScale(wrapper2);
+  		if (toCloseIcon2) {
+  			inAC(segmentD);
+  			inB(segmentE);
+  			inAC(segmentF);
+
+  			dummy2.className = 'dummy dummy--active';
+  		} else {
+  			outAC(segmentD);
+  			outB(segmentE);
+  			outAC(segmentF);
+
+  			dummy2.className = 'dummy';
+  		}
+  		toCloseIcon2 = !toCloseIcon2;
+  		setTimeout(function() {
+  			removeScale(wrapper2)
+  		}, 450);
+  	};
 
 })();
+
 
 
 //////////////form //////////////////
